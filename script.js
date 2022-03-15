e57401b6959df9777a3438ec21d2d71d939c6d5a
louisespicer <82650789+louisespicer@users.noreply.github.com>
2022-03-15 17:28:24 +0000

    main weather icon changes depending on weather

--- script.js
+++ script.js
@@ -29,20 +29,39 @@ dateElement.innerHTML = formatDate(currentTime);
 
 function displayTemperature(response) {
   console.log(response.data);
+
   let currentTemperature = document.querySelector("#temperature");
   let currentCity = document.querySelector("#city");
   let weatherDescription = document.querySelector("#description");
   let currentHumidity = document.querySelector("#humidity");
   let windSpeed = document.querySelector("#wind");
+  let currentIcon = document.querySelector("#icon");
+  let iconDescription = response.data.weather[0].main;
 
   currentTemperature.innerHTML = Math.round(response.data.main.temp);
   currentCity.innerHTML = response.data.name;
   weatherDescription.innerHTML = response.data.weather[0].description;
   currentHumidity.innerHTML = response.data.main.humidity + " %";
   windSpeed.innerHTML = response.data.wind.speed + " km/h";
+
+  if (iconDescription == "Thunderstorm") {
+    currentIcon.setAttribute("class", "fas fa-bolt-lightning main col");
+  } else if (iconDescription == "Drizzle") {
+    currentIcon.setAttribute("class", "fas fa-cloud-rain");
+  } else if (iconDescription == "Rain") {
+    currentIcon.setAttribute("class", "fas fa-cloud-showers-heavy");
+  } else if (iconDescription == "Snow") {
+    currentIcon.setAttribute("class", "fas fa-snowflake");
+  } else if (iconDescription == "Atmosphere") {
+    currentIcon.setAttribute("class", "fas fa-smog");
+  } else if (iconDescription == "Clear") {
+    currentIcon.setAttribute("class", "fas fa-sun");
+  } else if (iconDescription == "Clouds") {
+    currentIcon.setAttribute("class", "fas fa-cloud main col");
+  }
 }
 
+let city = "london";
 let apiKey = "e5128a69873aca4252645b70fe70eb05";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
-
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
